1bf83c2e62234f7e1b8b5af9f541dc8418d197d0
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-02-23 23:28:20 -0700

    improvements to predicates ui

--- public/predicateui.js
+++ public/predicateui.js
@@ -3,56 +3,66 @@ function PredicateUI(newpredbutton, newpreddialog, predcontainer, videoframe, jo
     this.newpredbutton = newpredbutton;
     this.newpreddialog = newpreddialog;
     this.predcontainer = predcontainer;
-    this.pred_by_id = {};
+    this.predname = {};
     
     this.setup = function() {
+        // create newpredbutton
         this.newpredbutton.button({
-            icons: {
-                primary: "ui-icon-plusthick",
-            },
+            icons: { primary: "ui-icon-plusthick" },
             disabled: false
         }).click(function() {
-            me.newpredicate();
+            me.newpreddialog.dialog('open');
         });
         
+        // create newpreddialog
         this.newpreddialog.dialog({
+            title: 'create a predicate',
             autoOpen: false,
-			height: 300,
-			width: 350,
-			modal: true,
-			resizable: false,
-			title: 'create a predicate',
-			buttons: {
-			    OK: function() {
-			        predicate_id = $('input[name="predicates"]:checked').val();
-			        if (!predicate_id) {
-			            alert('please select a predicate');
-			            return;
-			        }
-			        var new_pred = $('<p>' + me.pred_by_id[predicate_id] + 
-			                         ' ' + (me.predcontainer.children().length+1) + '</p>');
-		            me.predcontainer.append(new_pred);
-		            new_pred.effect('highlight', {}, 'slow');
-			        $(this).dialog('close');
-			    },
-			    Cancel: function() {
-			        $(this).dialog('close');
-			    }
-			}
-        });
-        
-        $.getJSON('/server/getpredicates', function(data) {
-            for (var p in data) {
-                me.pred_by_id[data[p][0]] = data[p][1];
-                me.newpreddialog.append('<input type="radio" name="predicates" id="' +
-                    data[p][0] + '" value="' + data[p][0] + '"><label for="' +
-                    data[p][0] + '">' + data[p][1] + '</label><br>');
+            modal: true,
+            height: 450,
+            width: 250,
+            buttons: {
+                ok: function() {
+                    // get the selected predicate type
+                    pred_id = $('input[name="predicates"]:checked').val();
+                    if (!pred_id) {
+                        alert('please select a predicate');
+                        return;
+                    }
+
+                    // add new predicate to the list
+                    var newprednum = me.predcontainer.children().length + 1;
+                    var newpredname = me.predname[pred_id];
+                    $('<div class="predblock"><div style="float:left">' + 
+                      newprednum + ' ' + newpredname +
+	              '</div><div style="float:right"><a href="#">add track</a></div><br></div>')
+                        .hide()
+                        .appendTo(me.predcontainer)
+                        .show('slow');
+
+                    // close dialog
+                    $(this).dialog('close');
+                },
+                cancel: function() {
+                    $(this).dialog('close');
+                }
             }
         });
-    }
-    
-    this.newpredicate = function() {
-        me.newpreddialog.dialog('open');
+        
+        // request list of predicates 
+        $.getJSON('/server/getpredicates', function(data) {
+            for (var p in data) {
+                var predid = data[p][0];
+                var predname = data[p][1];
+                // collect predicate ids and names
+                me.predname[predid] = predname;
+                // add radio buton to newpreddialog
+                me.newpreddialog.append('<input type="radio" name="predicates" id="' +
+                                        predid + '" value="' + predid +
+                                        '"><label for="' + predid + '">' +
+                                        predname + '</label><br>');
+            }
+        });
     }
     
     this.setup();
